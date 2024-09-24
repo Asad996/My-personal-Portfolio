a5392ccdba007595f788171d3cb5fe3ddde75572
Asad996 <asadabbas235431@gmail.com>
2024-09-24 12:23:16 -0700

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
-import HeroSection from './Components/HeroSection'
-import SummarySection from './Components/SummarySection'
-
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import React from 'react';
+import HeroSection from './Components/HeroSection';
+import SummarySection from './Components/SummarySection';
 import CarouselComponent from './Components/CarouselComponent';
 import Tools from './Components/Tools';
 import Contact from './Components/Contact';
+import Navbar from './Components/Navbar';
 
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const App = () => {
   return (
     <>
-   <HeroSection/>
-    <SummarySection/>
-      <CarouselComponent/>
-      <Tools/>
-      <Contact/>
-   
-  
+      <Navbar />
+      <section id="home">
+        <HeroSection />
+      </section>
+      <section id="about">
+        <SummarySection />
+      </section>
+      <section id="work">
+        <CarouselComponent />
+      </section>
+      <section id="tools">
+        <Tools />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
     </>
-   
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-scroll';
+import './Navbar.css'; // or './styles.css'
 
 const Navbar = () => {
   return (
     <nav className="fixed bottom-5 w-full flex justify-center z-50">
-      <ul className="bg-[#333] text-white flex space-x-8 px-10 py-3 rounded-full shadow-lg">
+      <ul className="bg-[#33333396] text-white flex space-x-8 px-10 py-3 rounded-2xl shadow-lg">
         <li className="cursor-pointer">
           <Link
             to="home"
